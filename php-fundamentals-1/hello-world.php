168fd870f96c535d3635fc5b24a62514f12509d3
sjt7911 <svetlio.toshkin@gmail.com>
2016-01-20 16:56:02 +0200

    Create hello-world.php

---
+++ php-fundamentals-1/hello-world.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello World</title>
+</head>
+<body>
+	<!-- PHP може да бъде написан на всякъде във вашия HTML документ -->
+	<!-- Когато искаме да изведем резултат или да изпишем нещо на HTML старницата, PHP тага и кодът трябва да са в <body> частта -->
+	<?php
+		// И echo и print извеждат / изписват резултат на HTML старницата.
+		echo "Hello world!!!"; // Echo е по-използвания и предпочитан синтаксис.
+		print "Hello world again!!!";
+	?>
+</body>
+</html>

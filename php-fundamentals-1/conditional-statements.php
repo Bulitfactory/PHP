26ff1c93e21305f93027df75dbd5429d2496343e
sjt7911 <svetlio.toshkin@gmail.com>
2016-01-27 15:08:32 +0200

    Update conditional-statements.php

--- php-fundamentals-1/conditional-statements.php
+++ php-fundamentals-1/conditional-statements.php
@@ -23,6 +23,23 @@
 
 		/* SWITCH
 		=============================================*/
+		$myFavoriteCar = "Aston Martin";
+
+		switch ($myFavoriteCar) {
+			case "Bmw":
+				echo "It's not my favorite car";
+				break;
+			case "Mercedes":
+				echo "It's not my favorite car";
+				break;
+			case "Aston Martin";
+				echo "Yee Aston Martin it's my favorite car";
+				break;
+			default:
+				echo "Your favorite car is not here!";
+				break;
+		}
+
 	?>
 </body>
 </html>

fe22c3c54af1185b2fdf8a06b980c774695a107d
sjt7911 <svetlio.toshkin@gmail.com>
2016-01-27 14:32:59 +0200

    Create conditional-statements.php

---
+++ php-fundamentals-1/conditional-statements.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>Условни опеаратори | Conditional statements</title>
+</head>
+<body>
+	<?php
+		/* IF ELSE ELSEIF
+		=============================================*/
+		$a = 5;
+		$b = 20;
+
+		if ($a == $b) {
+			echo $a." is equal to ".$b."<hr/>";
+		}
+		elseif ($a > $b) {
+			echo $a." is bigger than ".$b."<hr/>";
+		}
+		else {
+			echo $a." is smaller than ".$b."<hr/>";
+		}
+
+		/* SWITCH
+		=============================================*/
+	?>
+</body>
+</html>

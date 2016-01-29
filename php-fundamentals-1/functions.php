e81f73c868e54b77e650a593e49523bf57b2f44a
sjt7911 <svetlio.toshkin@gmail.com>
2016-01-29 12:13:27 +0200

    Update functions.php

--- php-fundamentals-1/functions.php
+++ php-fundamentals-1/functions.php
@@ -18,12 +18,27 @@
 		}
 		createDinamicHtmlTags(); // извикване на функция/изпълнение.
 
+
 		/* Функции с параметри / аргументи
 		======================================*/
 		function showFullName($firstname, $lastName) {
 			echo "My full name is: ".$firstname." ".$lastName;
 		}
 		showFullName("Svetoslav", "Toshkin");
+		
+		
+		/* Капсолиране на имената на променливите като ги дефинираме в тялото на функцията
+		===================================================================================*/
+		function fooArguments() {
+			$a = 5;
+			$b =10;
+		    $result = $a + $b;
+		    echo $result;
+		}
+		//echo $result; ако се опитаме да изведем $result ще получим грешка undefinded variable / недефинирана променлива.
+		// Единствения начин да достъпим променливата е като извикаме функцията.
+		echo fooArguments();
+		
 	?>
 </body>
 </html>

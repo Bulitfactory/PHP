977f7925277188c005a417240cf7d649a8551418
sjt7911 <svetlio.toshkin@gmail.com>
2016-02-08 11:04:02 +0200

    Create constants.php

---
+++ php-fundamentals-1/constants.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>Константи | Constants</title>
+</head>
+<body>
+	
+	<?php
+
+		// Дефинираме константа с define(name, value).
+		define("MYFIRSTNAME", "Svetoslav");
+		// Отпечатваме константата с нейното име.
+		echo MYFIRSTNAME;
+
+		// ЗАПОМНЕТЕ - стойноста на константата не може да бъде променяна / манипулирана при изпълнението на скрипта!!!
+
+	?>
+
+</body>
+</html>

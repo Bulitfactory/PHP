a12edd2f7b35787b94f17d761d13052814a107b9
sjt7911 <svetlio.toshkin@gmail.com>
2016-02-20 22:04:29 +0200

    Create select.php

---
+++ select-from-db/select.php
@@ -0,0 +1,21 @@
+<?php
+    $useremail = "svetlio.toshkin@gmail.com";
+
+    // Свързваме глобалната променлива $connection към prepare statement.
+    $stmt = $connection->prepare("SELECT useremail FROM users WHERE useremail = ?");
+
+    // bind_param -> свързава променливите към  като параметър.
+    $stmt->bind_param("s", $useremail);
+
+    // Изпълнява се заявката.
+    $stmt->execute();
+
+    // Свързваме резултата с променлива.
+    $stmt->bind_result($email);
+
+    // Достъпваме стойността.
+    $stmt->fetch();
+
+    echo "<br>"."Резултатът от селект заявката е: ".$email."<br>";
+
+?>

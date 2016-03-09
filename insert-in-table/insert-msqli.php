b9d06c8aad891baad0647e987c6c9334ae18bf70
sjt7911 <svetlio.toshkin@gmail.com>
2016-03-09 15:27:34 +0200

    Create insert-msqli.php

---
+++ insert-in-table/insert-msqli.php
@@ -0,0 +1,26 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "test";
+
+    // Създаваме връзка с базата данни
+    $connection = mysqli_connect($host, $user, $password, $database);
+    
+    if (mysqli_connect_errno()){
+        die("Нещо се обърка!!! Опитайте отново малко по късно.");
+    }
+    
+    $value = $_POST["key"];
+    
+    $createItem = $connection->query("INSERT INTO nameOfTheTable (columnName, ...) VALUES ('$value', ... )");
+    
+    if ($createItem == TRUE) {
+        echo "Успешен запис";
+    }
+    else {
+        echo "Нещо се обърка!!!";
+    }
+
+?>
